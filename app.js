6354554ab0c9a4844f223a87292e2a0fb1b75587
Jason Zhao <zhao473@purdue.edu>
2015-06-28 23:12:03 -0400

    Setting up dependencies and testing them out

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-var express = require('express'), sass = require('node-sass');
+var express = require('express'), sassMiddleware = require('node-sass-middleware');
 
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.logger('dev'));
 
+app.use(sassMiddleware({
+    src: __dirname,
+    dest: __dirname,
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/prefix'
+}));
+
+app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-	res.end('Hello, world!');
+	res.render('index', { 
+		title: 'Home' 
+	});
 });
 
 app.listen(3000);
